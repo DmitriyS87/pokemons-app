63d2a9cb82b305a72761e64eb88898956fce3cc1
DmitriyS87 <schukindmitriy@gmail.com>
2019-05-07 00:24:31 +0800

    Updates

---
+++ precache-manifest.1e6a3634b41fdf2b13300011f808172f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3344f9b6750e887d8359ee36507f7419",
+    "url": "/pokemons-app/index.html"
+  },
+  {
+    "revision": "b6dc1a3729f5f81feb13",
+    "url": "/pokemons-app/static/css/main.786804e4.chunk.css"
+  },
+  {
+    "revision": "eac1f60531fbc9a3c1b1",
+    "url": "/pokemons-app/static/js/2.8d8f6469.chunk.js"
+  },
+  {
+    "revision": "b6dc1a3729f5f81feb13",
+    "url": "/pokemons-app/static/js/main.7f92c475.chunk.js"
+  },
+  {
+    "revision": "eac8f90ef9016d48d9ed",
+    "url": "/pokemons-app/static/js/runtime~main.c6bae5c3.js"
+  }
+]);
